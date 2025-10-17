dd167eb00be3b6034e6e60fc47ae84c8076013da
Mohammed mutaman Abdalazeem Abbas Mus <87597397+mutamanabdo@users.noreply.github.com>
2025-10-17 07:18:50 +0000

    fixed urls

--- database/urls.py
+++ database/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import MemberListView, MemberDetailView, research4life_redirect
+from .views import MemberListView, MemberDetailView, research4life_access
 
 urlpatterns = [
     path('api/members/', MemberListView.as_view(), name='member-list'),
     path('api/members/<int:id>/', MemberDetailView.as_view(), name='member-detail'),
-    path('r4l/', research4life_redirect, name='research4life_redirect')
+    path('r4l/', research4life_access, name='research4life_access')
 ]
