aae0691423f7ecbb0b7ba167ba10ad6571178f1e
Mohammed mutaman Abdalazeem Abbas Mus <87597397+mutamanabdo@users.noreply.github.com>
2025-10-19 21:26:48 +0000

    fix

--- pso/settings.py
+++ pso/settings.py
@@ -27,7 +27,7 @@ se('tomato').as_sql
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY"])
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
